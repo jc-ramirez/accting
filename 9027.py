6d947e657a3e7be44ba1cf39b6058c847ebe2ef2
Juan Ramirez <juanramirez@juanramirezs-macbook.local>
2023-03-02 22:04:34 -0500

    separated the fuctions and added header creation

--- 9027.py
+++ 9027.py
@@ -1,111 +1,44 @@
-import openpyxl
-import datetime
-import string
+from resources import *
 
-from openpyxl import Workbook
-from openpyxl import load_workbook
-mth = datetime.datetime.now().strftime("%b")
-path = "9027.xlsx"
-
-#def open_workbook(path):
-  #wb = load_workbook(path)
-  #print(f"Worksheet names: {wb.sheetnames}")
-  #sheet = wb.active
-  #print(sheet)
-
-def firstmonth(wb, mth):
-  if mth in wb.sheetnames:
-    return False
-  else:
-    return True
-  
-
-def ld_rents(path, sheet):
-  #wb = load_workbook(path)
-  ws = wb[sheet]
-  mth = datetime.datetime.now().strftime("%b")
-  source = ws
-  target = wb.copy_worksheet(source)
-  target.title = mth
-  #wss = wb["Rents Copy"]
-  #print(wss.title)
-  #wss.title = mth
-  #wb["Feb"] = wb.copy_worksheet(ws)
-  print(f"Worksheet names: {wb.sheetnames}")
-
-
-def coord_conv(str):
-  if ":" not in str:
-    print("Please recheck coordinates.")
-    return 0
-  else:
-    x = str.split(":")
-    d = {"min_row" : int(x[0][1]), "min_col" : string.ascii_lowercase.index(x[0][0].lower()) + 1, "max_row" : int(x[1][1]) + 1, 
-    "max_col" : string.ascii_lowercase.index(x[1][0].lower()) + 1}
-    return d
-
-
-def cp_values(sheet_name, cell_range):
-  try:
-    sh = wb[sheet_name]
-    cdinates = coord_conv(cell_range)
-    d = {}
-
-    for i in range(cdinates['min_row'], cdinates['max_row']):
-      cell_value_aptno = sh.cell(i,cdinates['min_col']).value
-      cell_value_rent = sh.cell(i, cdinates['max_col']).value
-      d[cell_value_aptno] = cell_value_rent
-
-    for e in d:
-      print(f"{e} = {d[e]}")
-	
-  except:
-    print(f"'{sheet_name}' not found. Quitting.")
-    return
-
-  return d
-
-
-def wr_values(dict, sheet_name, cell_range):
-  try:
-    sh = wb[sheet_name]
-    cdinates = coord_conv(cell_range)
-    ind = 0
-
-    for i in range(cdinates['min_row'], cdinates['max_row']):
-        sh.cell(i,cdinates['min_col']).value = list(dict.keys())[ind]
-        sh.cell(i, cdinates['max_col']).value = list(dict.values())[ind]
-        ind += 1
-
-    for e in dict:
-      print(f"{e} = {dict[e]}")
-  
-  except:
-    print(f"'Couldn't write.")
-    return
-
-  return d
-
-def create_workbook(path):
-  wb = Workbook()
-  mth = datetime.datetime.now().strftime("%b")
-  #dttm = datetime.datetime.strptime(mylist[i], "%m/%d/%Y")
-  sheet = wb.active
-  sheet.cell(row=1,column=1).value = mth
-  wb.save(path)
+min_headers = ["cta or apt", "", "salida", "entrada"]
   
 if __name__ == "__main__":
+  opts = find_sheet()
+  
+  if len(opts) > 1:
+    path = input(f"Con que cuenta te gustaria trabajar {*opts,}?\n")
+    path = path + ".xlsx"
+
+  elif len(opts) == 1:
+    opciones =input(f"Si te gustaria habrir {opts[0]}, escribi, '{opts[0]}'' o si te gustraria comencar otra cuenta, escribi 'otra'.\n")
+    if opciones == opts[0]:
+      path = opts[0] + ".xlsx"
+    else:
+      path = input("Por favor entra el nombre para iniciar (ejemplo '9027'.)") + ".xlsx"
+
+  else:
+    path = input("No existen cuentas, vamos iniciar una. Por favor entra un nombre para iniciar (ejemplo '9027'.\n") + ".xlsx"
+    wb = openpyxl.Workbook()
+    wb.save(path)
+    hdrs = init_headers()
+    print(hdrs)
+
+    
+ 
   wb = load_workbook(path)
-  #ld_rents(wb, "Rents")
   if firstmonth(wb, mth):
-    st_month = input("Esta es la primera vez que inicias sesión este mes. Te gustaria empezar la cuenta mensual?\nEscribi 'si' o 'no'\n")
+    st_month = input("Esta es la primera vez que inicias sesión con esta cuenta este mes. Te gustaria empezar la cuenta mensual?\nEscribi 'si' o 'no'\n")
 
     if st_month.lower() == 'si':
-      #ts_month = ld_rents(path, "Rents")
-      d = cp_values("Rents", "A2:B5")
       ws = wb.create_sheet(f"{mth}")
+      wb.active = ws
+      for i in range(len(min_headers)):
+        create_col(ws, i + 1, min_headers[i])
+      #sheet = wb.active
+      wb.save(path)
+
       print(f"Worksheet names: {wb.sheetnames}")
-      wr_values(d, f"{mth}", "A2:B5")
+      #wr_values(d, f"{mth}", "A2:B5")
 
   else:
     st_month = input("Con que te puedeo ayudar?\n")
@@ -116,18 +49,3 @@ if __name__ == "__main__":
 
 
 
-
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
